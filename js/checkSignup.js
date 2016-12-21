232652681d46c559e6259245b3f8273f5bad1cee
canlanqianyang <1489368679@qq.com>
2016-12-21 20:11:37 +0800

    注册检验

--- js/checkSignup.js
+++ js/checkSignup.js
@@ -2,13 +2,25 @@
  * Created by hubeini on 2016/12/21.
  */
 
+var userNameError=false;
+var passWordError=false;
+var confirmError=false;
+var nickNmeError=false;
+var emailError=false;
+
 function check() {
     if (document.getElementById("inputPassword3").value !=
         document.getElementById("inputConfirm3").value) {
         document.getElementById("warning").innerHTML = "两次密码的输入不一致";
+        confirmError=true;
+        /*document.getElementById("signupButton").disabled=true;*/
+        //return false;
     } else {
-        document.getElementById("warning").innerHTML = "   ";
+        document.getElementById("warning").innerHTML = "";
+        confirmError=false;
+        //return true;
     }
+    checkForButton();
 }
 
 function checkNickname()
@@ -18,39 +30,56 @@ function checkNickname()
     var nickname=id.value;
     if(nickname.replace(/[^\x00-\xff]/g,"**").length > 15 ) {
         document.getElementById("warning2").innerHTML = "输入的用户名过长";
+        nickNmeError=true;
+        /*document.getElementById("signupButton").disabled=true;*/
+        //return false;
     }
     else{
-        document.getElementById("warning2").innerHTML = "   ";
+        document.getElementById("warning2").innerHTML = "";
+        nickNmeError=false;
+        //checkFinish();
+        //return true;
     }
+    checkForButton();
 }
 
 function checkUsername() {
     var username = document.getElementById("inputUsername3").value;
-//        if( username == "" || username == null ){
-//            alert( "请先填写用户名！" );
-//            return false;
-//        }
     switch( isUsername( username ) ){
         case 0: break;
         case 1: {
             changeUsernamePrompt( "格式不正确，用户名不能以数字开头" );
+            userNameError=true;
+            checkForButton();
+            //document.getElementById("signupButton").disabled=true;
             return false;
         }
         case 2: {
             changeUsernamePrompt( "字符长度有误，合法长度为6-20个字符" );
+            userNameError=true;
+            checkForButton();
+            //document.getElementById("signupButton").disabled=true;
             return false;
         }
         case 3: {
             changeUsernamePrompt( "含有非法字符，用户名只能包含_,英文字母，数字" );
+            userNameError=true;
+            checkForButton();
+            //document.getElementById("signupButton").disabled=true;
             return false;
         }
         case 4: {
             changeUsernamePrompt("格式不正确，用户名只能包含_,英文字母，数字" );
+            userNameError=true;
+            checkForButton();
+            //document.getElementById("signupButton").disabled=true;
             return false;
         }
     }
     document.getElementById("warning3").innerHTML = "   ";
-    return true;
+    userNameError=false;
+    //return true;
+    checkForButton();
 }
 function changeUsernamePrompt(cnt){
     document.getElementById( "warning3" ).innerHTML = cnt;
@@ -77,8 +106,73 @@ function checkPassword() {
     var password=id.value;
     if(password.length < 6){
         document.getElementById("warning4").innerHTML = "密码长度需大于6位";
+        passWordError=true;
+        //document.getElementById("signupButton").disabled=true;
+        //return false;
     }
     else {
         document.getElementById("warning4").innerHTML = "   ";
+        passWordError=false;
+        //checkFinish();
+        //return true;
+    }
+    checkForButton();
+}
+
+function checkEmail()
+{
+
+    var id = document.getElementById("inputEmail3");
+    var email = id.value;
+
+    var expr = /^([0-9]|[a-z])+@([0-9]|[a-z])+(\.[c][o][m])$/i;
+    if (!expr.test(email)) {
+        document.getElementById("warning5").innerHTML = "输入的邮箱格式有误";
+        emailError=true;
+        //document.getElementById("signupButton").disabled=true;
+        //return false;
+    }
+    else {
+        document.getElementById("warning5").innerHTML = " ";
+        emailError=false;
+        //checkFinish();
+        //return true;
+    }
+    checkForButton();
+}
+
+function checkFinish()
+{
+    var password = document.getElementById("inputPassword3").value;
+    var email = document.getElementById("inputEmail3").value;
+    var confirm= document.getElementById("inputConfirm3").value;
+    var nickname = document.getElementById("inputNickname3").value;
+    var username=document.getElementById("inputUsername3").value;
+    var isMale=document.getElementById("inlineRadio1").checked;
+    var isFemale=document.getElementById("inlineRadio2").checked;
+    if(username &&password && confirm && nickname&&email&&(isMale||isFemale)) {
+        return true;
+    }
+    else
+        return false;
+}
+
+function checkError(){
+   if(userNameError||passWordError||confirmError||nickNmeError||emailError)
+   {
+       return false;
+   }
+   else
+   {
+       return true;
+   }
+}
+function checkForButton(){
+    if(checkFinish()&&checkError()){
+        document.getElementById("signupButton").disabled=false;
+    }
+    else
+    {
+        document.getElementById("signupButton").disabled=true;
     }
 }
